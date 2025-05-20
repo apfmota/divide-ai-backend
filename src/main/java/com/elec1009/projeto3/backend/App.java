7f0d15a28892c0080c1db600c24273d5e87b3f57
Andriel Prieto <apmota@inf.ufsm.br>
2025-05-20 11:15:12 -0300

    chore: create maven project

---
+++ src/main/java/com/elec1009/projeto3/backend/App.java
@@ -0,0 +1,10 @@
+package com.elec1009.projeto3.backend;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
