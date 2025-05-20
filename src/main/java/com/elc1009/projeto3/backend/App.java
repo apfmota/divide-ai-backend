2d0d81be8d9f968d74e89ecab75f7b2fa008fe95
Andriel Prieto <apmota@inf.ufsm.br>
2025-05-20 11:36:17 -0300

    feat: create API

--- src/main/java/com/elc1009/projeto3/backend/App.java
+++ src/main/java/com/elc1009/projeto3/backend/App.java
@@ -1,10 +1,14 @@
 package com.elc1009.projeto3.backend;
+import io.javalin.Javalin;
 
 /**
  * Hello world!
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        var app = Javalin.create(/*config*/)
+            .get("/", ctx -> ctx.result("Hello World"));
+            //adicionar aqui as demais rotas
+        app.start(80);
     }
 }
