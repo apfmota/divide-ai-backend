937f6835bac8e31d13836d4a70581b7df992fa17
Andriel Prieto <apmota@inf.ufsm.br>
2025-05-20 19:17:54 -0300

    feat: switch to Spring Boot

---
+++ src/main/java/com/elc1009/projeto3/backend/DivideAiBackendApplication.java
@@ -0,0 +1,13 @@
+package com.elc1009.projeto3.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DivideAiBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DivideAiBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/elc1009/projeto3/backend/DivideAiBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elc1009.projeto3.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DivideAiBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
