da0c71f0396739902242cce9c340ad8d0eca77d7
Andriel Prieto <apmota@inf.ufsm.br>
2025-06-22 20:09:23 -0300

    chore: updating CORS

--- src/main/java/com/elc1009/projeto3/backend/CorsConfig.java
+++ src/main/java/com/elc1009/projeto3/backend/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173", "https://project3-2025a-andriel-joao.vercel.app")
+                .allowedOrigins("http://localhost:5173", "https://project3-2025a-andriel-joao.vercel.app", "https://project4-2025a-andriel-joao.vercel.app")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
